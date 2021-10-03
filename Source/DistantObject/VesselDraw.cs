ab9dee4d9a1960c3493a6936782e12bd33837039
Lisias T <me@lisias.net>
2021-10-03 08:14:52 -0300

    Using OnVesselWillDestroy instead of OnVesselDestroy on VesselDraw to prevent trying to draw a dead vessel, and some more adjustments on the vessel's life cycle handling on VesselDraw.

--- Source/DistantObject/VesselDraw.cs
+++ Source/DistantObject/VesselDraw.cs
@@ -33,31 +33,32 @@ namespace DistantObject
             }
         }
 
-		private static void VesselCheck(Vessel vessel)
+		private static void CheckDraw(Vessel vessel)
 		{
 			if (!vessel.loaded && Vector3d.Distance(vessel.GetWorldPos3D(), FlightGlobals.ship_position) < DistantObjectSettings.DistantVessel.maxDistance)
 			{
-				if (!meshEngineForVessel.ContainsKey(vessel))
-				{
-					Log.detail("Adding new definition for {0}", vessel.vesselName);
-					meshEngineForVessel[vessel] = Contract.MeshEngine.CreateFor(vessel);
-				}
+				VesselCheck(vessel);
 				meshEngineForVessel[vessel].Draw();
 			}
 			else
 				CheckErase(vessel);
 		}
 
-		private static void VesselLazyCheck(Vessel vessel)
+		private static void LazyCheckDraw(Vessel vessel)
+		{
+			VesselCheck(vessel);
+			if (!vessel.loaded && Vector3d.Distance(vessel.GetWorldPos3D(), FlightGlobals.ship_position) < DistantObjectSettings.DistantVessel.maxDistance)
+			{
+				meshEngineForVessel[vessel].Draw();
+			}
+		}
+
+		private static void VesselCheck(Vessel vessel)
 		{
 			if (!meshEngineForVessel.ContainsKey(vessel))
 			{
 				Log.detail("Adding new definition for {0}", vessel.vesselName);
 				meshEngineForVessel[vessel] = Contract.MeshEngine.CreateFor(vessel);
-			}
-			if (Vector3d.Distance(vessel.GetWorldPos3D(), FlightGlobals.ship_position) < DistantObjectSettings.DistantVessel.maxDistance && !vessel.loaded)
-			{
-				meshEngineForVessel[vessel].Draw();
 			}
 		}
 
@@ -74,7 +75,7 @@ namespace DistantObject
 						if (target.GetType().Name == "Vessel")
 						{
 							workingTarget = FlightGlobals.Vessels.Find(index => index.GetName() == target.GetName());
-							VesselCheck(workingTarget);
+							CheckDraw(workingTarget);
 						}
 						else if (workingTarget != null)
 							CheckErase(workingTarget);
@@ -90,7 +91,7 @@ namespace DistantObject
 						n = 0;
 
 					if (FlightGlobals.Vessels[n].vesselType != VesselType.Flag && FlightGlobals.Vessels[n].vesselType != VesselType.EVA && (FlightGlobals.Vessels[n].vesselType != VesselType.Debris || !DistantObjectSettings.DistantVessel.ignoreDebris))
-						VesselCheck(FlightGlobals.Vessels[n]);
+						CheckDraw(FlightGlobals.Vessels[n]);
 				} break;
 
 				case DistantObjectSettings.ERenderMode.RenderAllDontForget:
@@ -100,7 +101,7 @@ namespace DistantObject
 						n = 0;
 
 					if (FlightGlobals.Vessels[n].vesselType != VesselType.Flag && FlightGlobals.Vessels[n].vesselType != VesselType.EVA && (FlightGlobals.Vessels[n].vesselType != VesselType.Debris || !DistantObjectSettings.DistantVessel.ignoreDebris))
-						VesselLazyCheck(FlightGlobals.Vessels[n]);
+						LazyCheckDraw(FlightGlobals.Vessels[n]);
 				} break;
 			}
 		}
@@ -121,9 +122,10 @@ namespace DistantObject
 		private void Start()
 		{
 			GameEvents.onGameSceneSwitchRequested.Add(this.OnGameSceneSwitchRequested);
-			GameEvents.onVesselCreate.Add(this.OnVesselCreate);
 			GameEvents.onVesselChange.Add(this.OnVesselChange);
-			GameEvents.onVesselDestroy.Add(this.OnVesselDestroy);
+			GameEvents.onVesselGoOnRails.Add(this.OnVesselGoOnRails);
+			GameEvents.onVesselGoOffRails.Add(this.OnVesselGoOffRails);
+			GameEvents.onVesselWillDestroy.Add(this.OnVesselWillDestroy);
 			DistantObjectSettings.Commit();
 		}
 
@@ -131,9 +133,10 @@ namespace DistantObject
 		private void OnDestroy()
 		{
 			Log.dbg("VesselDraw OnDestroy");
-			GameEvents.onVesselDestroy.Remove(this.OnVesselDestroy);
+			GameEvents.onVesselWillDestroy.Remove(this.OnVesselWillDestroy);
+			GameEvents.onVesselGoOffRails.Add(this.OnVesselGoOffRails);
+			GameEvents.onVesselGoOnRails.Add(this.OnVesselGoOnRails);
 			GameEvents.onVesselChange.Remove(this.OnVesselChange);
-			GameEvents.onVesselCreate.Remove(this.OnVesselCreate);
 			GameEvents.onGameSceneSwitchRequested.Remove(this.OnGameSceneSwitchRequested);
 			INSTANCE = null;
 		}
@@ -174,23 +177,26 @@ namespace DistantObject
 			else if (data.to.Equals(GameScenes.FLIGHT))	this.SetActiveTo(DistantObjectSettings.DistantVessel.renderVessels);
 		}
 
-		private void OnVesselCreate(Vessel vessel)
-		{
-			Log.dbg("Vessel {0} was Created.", vessel.vesselName);
-			if (DistantObjectSettings.DistantVessel.renderMode >= DistantObjectSettings.ERenderMode.RenderAllDontForget && vessel.GetType().Name == "Vessel")
-			{
-				Log.detail("Adding new definition for {0}", vessel.vesselName);
-				meshEngineForVessel[vessel] = Contract.MeshEngine.CreateFor(vessel);
-			}
-		}
-
 		private void OnVesselChange(Vessel vessel)
 		{
 			Log.dbg("Vessel {0} was Changeg.", vessel.vesselName);
 			CheckErase(vessel);	// Current meshes are invalid, we need to reaload them later.
 		}
 
-		private void OnVesselDestroy(Vessel vessel)
+		private void OnVesselGoOnRails(Vessel vessel)
+		{
+			Log.dbg("Vessel {0} Gone ON Rails.", vessel.vesselName);
+			if (DistantObjectSettings.DistantVessel.renderMode >= DistantObjectSettings.ERenderMode.RenderAllDontForget && vessel.GetType().Name == "Vessel")
+				VesselCheck(vessel);
+		}
+
+		private void OnVesselGoOffRails(Vessel vessel)
+		{
+			Log.dbg("Vessel {0} Gone OFF Rails.", vessel.vesselName);
+			CheckErase(vessel);	// Current meshes are invalid, we need to reaload them later.
+		}
+
+		private void OnVesselWillDestroy(Vessel vessel)
 		{
 			Log.dbg("Vessel {0} was Destroyed.", vessel.vesselName);
 			if (vessel.Equals(workingTarget)) workingTarget = null;
